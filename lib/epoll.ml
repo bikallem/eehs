37855dfa6014a7def8d31ed6c43e4d166e835eee
Bikal Lem <gbikal@gmail.com>
2022-11-05 13:06:25 +0000

    eio: add eio interface

--- lib/epoll.ml
+++ lib/epoll.ml
@@ -1,67 +1,60 @@
 type epoll_events = Cstruct.buffer
 type timeout_ms = int
 type maxevents = int
+type epoll_fd
 
-module C_epoll = struct
-  type epoll_fd
+module Op : sig
+  type t
 
-  module Events : sig
-    type t
+  val op_add : t
+  val op_mod : t
+  val op_del : t
+end = struct
+  type t = int
 
-    val ( + ) : t -> t -> t
-    val mem : t -> t -> bool
-    val is_none : t -> bool
-    val none : t
-    val epollin : t
-    val epollout : t
-    val epollpri : t
-    val epollerr : t
-    val epollhup : t
-    val epollet : t
-    val epolloneshot : t
-  end = struct
-    type t = int
+  let op_add = Config.epoll_ctl_add
+  let op_mod = Config.epoll_ctl_mod
+  let op_del = Config.epoll_ctl_del
+end
 
-    include Config
+external epoll_create : unit -> epoll_fd = "caml_epoll_create1"
 
-    let none = 0
-    let ( + ) = Int.logor
-    let mem a b = Int.logand a b = a
-    let is_none t = t = 0
-  end
+external epoll_ctl : epoll_fd -> Op.t -> Unix.file_descr -> int -> unit
+  = "caml_epoll_ctl"
 
-  module Op : sig
-    type t
+external epoll_wait : epoll_fd -> epoll_events -> maxevents -> timeout_ms -> int
+  = "caml_epoll_wait"
 
-    val op_add : t
-    val op_mod : t
-    val op_del : t
-  end = struct
-    type t = int
+module Io_events = struct
+  type t = int
 
-    let op_add = Config.epoll_ctl_add
-    let op_mod = Config.epoll_ctl_mod
-    let op_del = Config.epoll_ctl_del
-  end
-
-  external epoll_create : unit -> epoll_fd = "caml_epoll_create1"
-
-  external epoll_ctl : epoll_fd -> Op.t -> Unix.file_descr -> Events.t -> unit
-    = "caml_epoll_ctl"
-
-  external epoll_wait :
-    epoll_fd -> epoll_events -> maxevents -> timeout_ms -> int
-    = "caml_epoll_wait"
+  let add = ( lor )
+  let remove a b = a land lnot b
+  let readable = Config.epollin
+  let writable = Config.epollout
+  let is_readable t = t land readable = t
+  let is_writable t = t land writable = t
 end
 
 type t =
-  { epollfd: C_epoll.epoll_fd
-  ; max_ready_events: int
+  { epollfd: epoll_fd
+  ; maxevents: maxevents
   ; epoll_events: Cstruct.t
   ; mutable num_ready_events: int }
 
-let create max_ready_events =
-  { epollfd= C_epoll.epoll_create ()
-  ; max_ready_events
-  ; epoll_events= Cstruct.create (Config.sizeof_epoll_event * max_ready_events)
+let create maxevents =
+  { epollfd= epoll_create ()
+  ; maxevents
+  ; epoll_events= Cstruct.create (Config.sizeof_epoll_event * maxevents)
   ; num_ready_events= 0 }
+
+let add t fd io_events = epoll_ctl t.epollfd Op.op_add fd io_events
+let modify t fd io_events = epoll_ctl t.epollfd Op.op_mod fd io_events
+let remove t fd : unit = epoll_ctl t.epollfd Op.op_del fd 0
+
+let poll_io ?(timeout_ms = 0) (t : t) : [`Ok | `Timeout] =
+  t.num_ready_events <-
+    epoll_wait t.epollfd
+      (Cstruct.to_bigarray t.epoll_events)
+      t.maxevents timeout_ms ;
+  if t.num_ready_events = 0 then `Timeout else `Ok
---
+++ lib/epoll.mli
@@ -0,0 +1,18 @@
+module Io_events : sig
+  type t
+
+  val readable : t
+  val writable : t
+  val add : t -> t -> t
+  val remove : t -> t -> t
+  val is_readable : t -> bool
+  val is_writable : t -> bool
+end
+
+type t
+
+val create : int -> t
+val add : t -> Unix.file_descr -> Io_events.t -> unit
+val modify : t -> Unix.file_descr -> Io_events.t -> unit
+val remove : t -> Unix.file_descr -> unit
+val poll_io : ?timeout_ms:int -> t -> [`Ok | `Timeout]
