4ca9b243fe73e0e2ca6f70aafd622ab30cb90b9e
Bikal Lem <gbikal@gmail.com>
2024-02-23 12:02:37 +0000

    bin: use  readable/writeable to determine echo messages

--- bin/tcp_echo_server.ml
+++ bin/tcp_echo_server.ml
@@ -1,35 +1,73 @@
 open Eehs
 
+type client = {
+  read_buf : Bytes.t; (* read buffer *)
+  epoll : Epoll.t;
+  addr : Unix.sockaddr; (* client address *)
+  fd : Unix.file_descr; (* client file descripter for read/write *)
+  mutable fd_closed : bool;
+  mutable read_offset : int;
+      (* count of bytes in read_buf available. set by read syscall *)
+}
+
+type callback = Epoll.Io_events.t -> unit
+
+let initial_buffer_size = 0x1000 (* 4K *)
+
 let pp_sockaddr fmt = function
   | Unix.ADDR_UNIX _ -> failwith "Unix domain socket not supported"
   | ADDR_INET (addr, port) ->
     Format.fprintf fmt "%s.%d" (Unix.string_of_inet_addr addr) port
 
-let clientfd_cb (epoll : Epoll.t) (_ : Unix.sockaddr)
-    (client_fd, (_ : Epoll.Io_events.t)) =
-  let buflen = 6 in
-  let buf = Bytes.create buflen in
-  let offset = 0 in
-  match Epoll.read client_fd buf offset (buflen - offset) with
-  | got when got = 0 ->
-    Epoll.remove epoll client_fd;
-    Unix.close client_fd
-  | got ->
-    Printf.printf "\ngot: %d, %S%!" got (Bytes.unsafe_to_string buf);
-    ignore (Unix.write client_fd buf offset got : int);
+let traceln fmt = Format.(fprintf std_formatter ("\n+" ^^ fmt ^^ "%!"))
 
-    let ret = Epoll.read client_fd buf offset (buflen - offset) in
-    Printf.printf "\nret: %d%!" ret
-  | exception Unix.Unix_error ((EAGAIN | EWOULDBLOCK), _, _) -> ()
+let client_cb (client : client) (ev : Epoll.Io_events.t) =
+  let readable = Epoll.Io_events.readable ev in
+  let writable = Epoll.Io_events.writable ev in
+  let len = Bytes.length client.read_buf - client.read_offset in
+  traceln
+    "client_cb: readable: %b, writable: %b, fd_closed: %b, len: %d, ev: %o"
+    readable writable client.fd_closed len
+    (ev :> int);
 
-let serverfd_cb (epoll : Epoll.t) fdcb (server_fd, (_ : Epoll.Io_events.t)) =
-  let client_fd, client_addr = Epoll.accept4 server_fd in
+  if readable && (not client.fd_closed) && len > 0 then (
+    traceln "reading ..";
+    match Epoll.read client.fd client.read_buf client.read_offset len with
+    | got when got = 0 ->
+      Epoll.remove client.epoll client.fd;
+      Unix.close client.fd;
+      client.fd_closed <- true
+    | got when got > 0 ->
+      let rcvdbytes = Bytes.sub client.read_buf client.read_offset got in
+      client.read_offset <- client.read_offset + got;
+      traceln "\ngot: %d bytes, %S" got (Bytes.unsafe_to_string rcvdbytes)
+    | _ -> Error.raise_syscall_error "read");
 
-  Format.(fprintf std_formatter "\nConnected to %a%!" pp_sockaddr client_addr);
+  if (not client.fd_closed) && client.read_offset > 0 then (
+    traceln "writing ...";
+    (* TODO check for EGAIN/EWOULDBLOCK *)
+    let wrote = Unix.write client.fd client.read_buf 0 client.read_offset in
+    client.read_offset <- client.read_offset - wrote;
+    traceln "\nwrote: %d bytes" wrote)
 
-  Epoll.add epoll client_fd Epoll.Io_events.read;
-  let clientfd_cb = clientfd_cb epoll client_addr in
-  Hashtbl.replace fdcb client_fd clientfd_cb
+let server_cb (epoll : Epoll.t) (fdcb : (Unix.file_descr, callback) Hashtbl.t)
+    server_fd (_ : Epoll.Io_events.t) =
+  let fd, addr = Epoll.accept4 server_fd in
+  let client =
+    {
+      read_buf = Bytes.create initial_buffer_size (* 4K *);
+      epoll;
+      addr;
+      fd;
+      fd_closed = false;
+      read_offset = 0;
+    }
+  in
+  traceln "Connected to %a" pp_sockaddr client.addr;
+
+  Epoll.add epoll client.fd Epoll.Io_events.read;
+  let client_cb = client_cb client in
+  Hashtbl.replace fdcb client.fd client_cb
 
 let max_connections = 128
 
@@ -44,16 +82,17 @@ let () =
   Unix.listen server_fd max_connections;
 
   let epoll = Epoll.create max_connections in
-  Epoll.add epoll server_fd Epoll.Io_events.read;
+  Epoll.add epoll server_fd Epoll.Io_events.rw;
 
   (* hashtble with mapping {file_descr => callback} *)
   let fdcb = Hashtbl.create max_connections in
-  Hashtbl.replace fdcb server_fd (serverfd_cb epoll fdcb);
+  let server_cb = server_cb epoll fdcb server_fd in
+  Hashtbl.replace fdcb server_fd server_cb;
 
   (* Run epoll/accept loop *)
   while true do
-    Epoll.epoll_wait ~timeout_ms:(-1) epoll;
+    Epoll.epoll_wait ~timeout_ms:1000 epoll;
     Epoll.iter epoll (fun fd io_events ->
         let cb = Hashtbl.find fdcb fd in
-        cb (fd, io_events))
+        cb io_events)
   done
