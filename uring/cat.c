04e67d0204a5e3ad793cbbe1540847a78c874458
Bikal Lem <gbikal@gmail.com>
2024-02-27 20:51:33 +0000

    uring: fix cat

--- uring/cat.c
+++ uring/cat.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <string.h>
 
-#define BUFFER_SIZE 50
+#define BUFFER_SIZE 80
 
 int main(int argc, char *argv[])
 {
@@ -19,47 +20,38 @@ int main(int argc, char *argv[])
 	fd = open("hello.txt", O_NONBLOCK | O_RDONLY);
 	io_uring_queue_init(10, &ring, 0);
 
-	if (NULL == (sqe = io_uring_get_sqe(&ring)))
-		perror("io_uring_get_sqe() failed.");
-
-	io_uring_prep_read(sqe, fd, buf, BUFFER_SIZE, -1);
-
 	/* wait for the sqe to complete. */
 	/* __kernel_timespec */
 
 	for (;;) {
+		if (NULL == (sqe = io_uring_get_sqe(&ring)))
+			perror("io_uring_get_sqe() failed.");
+
+		io_uring_prep_read(sqe, fd, buf, BUFFER_SIZE, got);
 		ret = io_uring_submit_and_wait(&ring, 1);
 		if (ret <= 0) {
 			perror("io_uring_submit_and_wait() failed");
 			break;
 		}
 
-		/* printf("\nret: %d", ret); */
-
 		if (io_uring_wait_cqe(&ring, &cqe) != 0) {
 			perror("io_uring_wait_cqe() failed");
 			break;
 		}
 
+		io_uring_cqe_seen(&ring, cqe);
 		if (cqe->res == 0) {
 			printf("\nCOMPLETE");
 			break;
 		}
 
-		/* prepare next read request. */
-		if (NULL == (sqe = io_uring_get_sqe(&ring)))
-			perror("io_uring_get_sqe() failed.");
+		got += cqe->res;
+		printf("\n%.*s", BUFFER_SIZE, buf);
+		fflush(stdout);
+		memset(buf, '\000', BUFFER_SIZE);
 
-		if (cqe->res == BUFFER_SIZE) {
-			printf("\n%.*s", BUFFER_SIZE, buf);
-			fflush(stdout);
-
-			got += cqe->res;
-			io_uring_prep_read(sqe, fd, buf, BUFFER_SIZE, got);
-			io_uring_cqe_seen(&ring, cqe);
-		} else {
+		if (cqe->res < BUFFER_SIZE)
 			break;
-		}
 	}
 
 	close(fd);
